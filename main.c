e81734698a2dfdc93a476251b2467deced6069f9
pengdahong1225 <2102533303@qq.com>
2022-06-17 00:30:34 +0800

    2022.6.17

--- main.c
+++ main.c
@@ -26,89 +26,375 @@
 #include <st7789vw.h>
 #include <os_list.h>
 #include <pin.h>
-#include <os_event.h>
+#include <os_mq.h>
+#include <stdlib.h>
 
-#define LIST_TASK_PRIO 5
-#define LIST_STK_SIZE 1024
+#define LIST_TASK_PRIO 4
+#define LIST_STK_SIZE 2048
 
-os_event_t* event = OS_NULL;
+#define R 10
 
-/* task_key */
-static void Funtask_key(void *parameter)
+os_sem_t *sem = OS_NULL;
+os_mq_t* mq = OS_NULL;
+typedef struct key_data
 {
-		os_ubase_t count = 0;
-		os_uint32_t recved_set = 0;
-		while(1)
-		{
-			os_event_recv(event,0x01|0x02|0x04,OS_EVENT_OPTION_OR|OS_EVENT_OPTION_CLEAR,OS_WAIT_FOREVER,&recved_set);
-			
-			
-			
-			/*switch(rbuf.name)
-			{
-				case 'A':
-				{
-					os_kprintf("key0,lcd : RED\r\n");
-					lcd_clear(WHITE);
-					lcd_fill(10,100,100,230,RED);
-					break;
-				}
-				case 'B':
-				{
-					os_kprintf("key1,lcd : GREEN\r\n");
-					lcd_clear(WHITE);
-					lcd_fill(10,100,100,230,GREEN);
-					break;
-				}
-				case 'C':
-				{
-					os_kprintf("key2,lcd : BLUE\r\n");
-					lcd_clear(WHITE);
-					lcd_fill(10,100,100,230,BLUE);
-					break;
-				}
-				default:
-					break;
-			}*/
-			os_task_msleep(500);
-		}
+  char message;
+}key_data_t;
+
+/* 节点坐标信息 */
+struct node{
+	os_uint16_t x;
+	os_uint16_t y;
+};
+struct node snake_info[15];    /* 蛇节点最多15个 */
+static os_uint16_t count = 0;  /* 长度 */
+
+struct node* snode = OS_NULL;
+
+os_uint16_t xnum[9] = {40,140,100,80,60,180,200,120,160};
+os_uint16_t ynum[9] = {160,120,200,180,60,80,100,140,40};
+
+void create_node(struct node* node)
+{
+	srand(time(NULL));
+	int i = rand()%8;
+	int j = rand()%8;
+	node->x = xnum[i];
+	node->y = ynum[j];
 }
 
-/* task_user */
-static void Funtask_user(void *parameter)
+void left()
 {
-		while(1)
+	os_kprintf("left\r\n");
+	if(os_sem_wait(sem,OS_WAIT_FOREVER)==OS_EOK)
+	{
+		/* 计算所有节点 */
+		struct node pre_node = snake_info[0];
+		for(os_uint16_t i = 0;i<count;i++)
 		{
-			if(!os_pin_read(key_table[0].pin))
+			if(i==0)/* 头 */
 			{
-				os_kprintf("key0\r\n");
-				os_event_send(event,0x01);
+				if(snake_info[i].x - (R*2) >= 0)
+				{
+					snake_info[i].x = snake_info[i].x - (R*2);
+					if(snake_info[i].x == snode->x && snake_info[i].y == snode->y)/* 吃到点 */
+					{
+						os_kprintf("eat food\r\n");
+						free(snode);
+						snode = OS_NULL;
+						count++;
+					}
+				}
+				else
+				{
+					os_kprintf("game over\r\n");
+					exit(0);
+				}
 			}
-			if(os_pin_read(key_table[1].pin))
+			else
 			{
-				os_kprintf("key1\r\n");
-				os_event_send(event,0x02);
+				/* 身体 */
+				struct node temp_node = snake_info[i];
+				snake_info[i].x = pre_node.x;
+				snake_info[i].y = pre_node.y;
+				pre_node = temp_node;
 			}
-			if(!os_pin_read(key_table[2].pin))
-			{
-				os_kprintf("key2\r\n");
-				os_event_send(event,0x04);
-			}
-			os_task_msleep(200);
 		}
+	}
+	os_sem_post(sem);
+	/* 绘制所有节点 */
+	lcd_clear(YELLOW);
+	lcd_set_color(YELLOW,RED);
+	lcd_draw_rectangle(0,0,240,240);
+	lcd_set_color(BLUE,BLUE);
+	for(os_uint16_t i=0;i<count;i++)
+	{
+		lcd_draw_circle(snake_info[i].x,snake_info[i].y,R);
+		os_kprintf("x:%d y:%d\r\n",snake_info[i].x,snake_info[i].y);
+	}
+	
+	/* 随机节点 */
+	lcd_set_color(GRAY,GRAY);
+	if(snode == OS_NULL)
+	{
+		snode = (struct node*)os_malloc(sizeof(struct node));
+		create_node(snode);
+		lcd_draw_circle(snode->x,snode->y,R);
+	}
+	else
+		lcd_draw_circle(snode->x,snode->y,R);
+}
+void right()
+{
+	os_kprintf("right\r\n");
+	/* 计算所有节点 */
+	struct node pre_node = snake_info[0];
+	for(os_uint16_t i = 0;i<count;i++)
+	{
+		if(i==0)/* 头 */
+		{
+			if(snake_info[i].x + (R*2) <= 240)
+			{
+				snake_info[i].x = snake_info[i].x + (R*2);
+				if(snake_info[i].x == snode->x && snake_info[i].y == snode->y)/* 吃到点 */
+				{
+					os_kprintf("eat food\r\n");
+					free(snode);
+					snode = OS_NULL;
+					count++;
+				}
+			}
+			else
+			{
+				os_kprintf("game over\r\n");
+				exit(0);
+			}
+		}
+		else
+		{
+			/* 身体 */
+			struct node temp_node = snake_info[i];
+			snake_info[i].x = pre_node.x;
+			snake_info[i].y = pre_node.y;
+			pre_node = temp_node;
+		}
+	}
+	/* 绘制所有节点 */
+	lcd_clear(YELLOW);
+	lcd_set_color(YELLOW,RED);
+	lcd_draw_rectangle(0,0,240,240);
+	lcd_set_color(BLUE,BLUE);
+	for(os_uint16_t i=0;i<count;i++)
+	{
+		lcd_draw_circle(snake_info[i].x,snake_info[i].y,R);
+		os_kprintf("x:%d y:%d\r\n",snake_info[i].x,snake_info[i].y);
+	}
+	/* 随机节点 */
+	lcd_set_color(GRAY,GRAY);
+	if(snode == OS_NULL)
+	{
+		snode = (struct node*)os_malloc(sizeof(struct node));
+		create_node(snode);
+		lcd_draw_circle(snode->x,snode->y,R);
+	}
+	else
+		lcd_draw_circle(snode->x,snode->y,R);
+}
+void up()
+{
+	os_kprintf("up\r\n");
+	/* 计算所有节点 */
+	struct node pre_node = snake_info[0];
+	for(os_uint16_t i = 0;i<count;i++)
+	{
+		if(i==0)/* 头 */
+		{
+			if(snake_info[i].y - (R*2) >= 0)
+			{
+				snake_info[i].y = snake_info[i].y - (R*2);
+				if(snake_info[i].x == snode->x && snake_info[i].y == snode->y)/* 吃到点 */
+				{
+					os_kprintf("eat food\r\n");
+					free(snode);
+					snode = OS_NULL;
+					count++;
+				}
+			}
+			else
+			{
+				os_kprintf("game over\r\n");
+				exit(0);
+			}
+		}
+		else
+		{
+			/* 身体 */
+			struct node temp_node = snake_info[i];
+			snake_info[i].x = pre_node.x;
+			snake_info[i].y = pre_node.y;
+			pre_node = temp_node;
+		}
+	}
+	/* 绘制所有节点 */
+	lcd_clear(YELLOW);
+	lcd_set_color(YELLOW,RED);
+	lcd_draw_rectangle(0,0,240,240);
+	lcd_set_color(BLUE,BLUE);
+	for(os_uint16_t i=0;i<count;i++)
+	{
+		lcd_draw_circle(snake_info[i].x,snake_info[i].y,R);
+		os_kprintf("x:%d y:%d\r\n",snake_info[i].x,snake_info[i].y);
+	}
+	/* 随机节点 */
+	lcd_set_color(GRAY,GRAY);
+	if(snode == OS_NULL)
+	{
+		snode = (struct node*)os_malloc(sizeof(struct node));
+		create_node(snode);
+		lcd_draw_circle(snode->x,snode->y,R);
+	}
+	else
+		lcd_draw_circle(snode->x,snode->y,R);
+}
+void down()
+{
+	os_kprintf("down\r\n");
+	/* 计算所有节点 */
+	struct node pre_node = snake_info[0];
+	for(os_uint16_t i = 0;i<count;i++)
+	{
+		if(i==0)/* 头 */
+		{
+			if(snake_info[i].y + (R*2) <= 240)
+			{
+				snake_info[i].y = snake_info[i].y + (R*2);
+				if(snake_info[i].x == snode->x && snake_info[i].y == snode->y)/* 吃到点 */
+				{
+					os_kprintf("eat food\r\n");
+					free(snode);
+					snode = OS_NULL;
+					count++;
+				}
+			}
+			else
+			{
+				os_kprintf("game over\r\n");
+				exit(0);
+			}
+		}
+		else
+		{
+			/* 身体 */
+			struct node temp_node = snake_info[i];
+			snake_info[i].x = pre_node.x;
+			snake_info[i].y = pre_node.y;
+			pre_node = temp_node;
+		}
+	}
+	/* 绘制所有节点 */
+	lcd_clear(YELLOW);
+	lcd_set_color(YELLOW,RED);
+	lcd_draw_rectangle(0,0,240,240);
+	lcd_set_color(BLUE,BLUE);
+	for(os_uint16_t i=0;i<count;i++)
+	{
+		lcd_draw_circle(snake_info[i].x,snake_info[i].y,R);
+		os_kprintf("x:%d y:%d\r\n",snake_info[i].x,snake_info[i].y);
+	}
+	/* 随机节点 */
+	lcd_set_color(GRAY,GRAY);
+	if(snode == OS_NULL)
+	{
+		snode = (struct node*)os_malloc(sizeof(struct node));
+		create_node(snode);
+		lcd_draw_circle(snode->x,snode->y,R);
+	}
+	else
+		lcd_draw_circle(snode->x,snode->y,R);
+}
+
+/* 刷新显示 */
+static void show_taskfun(void *parameter)
+{
+	/* 初始化 */
+	lcd_clear(YELLOW);
+	lcd_set_color(YELLOW,RED);
+	lcd_draw_rectangle(0,0,240,240);
+	lcd_set_color(BLUE,BLUE);
+	lcd_draw_circle(240/2,240/2,R);
+	snake_info[0].x = 240/2;
+	snake_info[0].y = 240/2;
+	count = 1;
+	os_task_msleep(3000);
+	int flag = 0;
+	os_kprintf("Game start\r\n");
+
+	struct key_data buf;
+	os_size_t recv_size;
+	/* 初始化起点和随机点 */
+	if(snode == OS_NULL)
+	{
+		snode = (struct node*)os_malloc(sizeof(struct node));
+		create_node(snode);
+		lcd_clear(YELLOW);
+		lcd_set_color(YELLOW,RED);
+		lcd_draw_rectangle(0,0,240,240);
+		lcd_set_color(BLUE,BLUE);
+		for(os_uint16_t i = 0;i<count;i++)
+			lcd_draw_circle(snake_info[i].x,snake_info[i].y,R);
+		lcd_set_color(GRAY,GRAY);
+		lcd_draw_circle(snode->x,snode->y,R);
+	}
+	while(1)
+	{
+		/* 按键转向移动 */
+		if(OS_EOK == os_mq_recv(mq,&buf, sizeof(struct key_data), OS_NO_WAIT, &recv_size)){
+			if(buf.message=='A')
+				left();
+			else if(buf.message=='B')
+				right();
+			else if(buf.message=='C')
+				up();
+			else
+				down();
+			flag = 1;
+		}
+		/* 自方向移动 */
+		if(flag)
+		{
+			if(buf.message=='A')
+				left();
+			else if(buf.message=='B')
+				right();
+			else if(buf.message=='C')
+				up();
+			else
+				down();
+		}
+		os_task_msleep(500);
+	}
+}
+
+/* 按键任务 */
+static void key_taskfun(void *parameter)
+{
+	struct key_data a,b,c,d;
+	a.message='A';
+	b.message='B';
+	c.message='C';
+	d.message='D';
+	while(1)
+	{
+		if(!os_pin_read(key_table[0].pin))
+		{
+			os_mq_send(mq,&a, sizeof(struct key_data),OS_WAIT_FOREVER);
+		}
+		if(os_pin_read(key_table[1].pin))
+		{
+			os_mq_send(mq,&d, sizeof(struct key_data),OS_WAIT_FOREVER);
+		}
+		if(!os_pin_read(key_table[2].pin))
+		{
+			os_mq_send(mq,&b, sizeof(struct key_data),OS_WAIT_FOREVER);
+		}
+		if(!os_pin_read(key_table[3].pin))
+		{
+			os_mq_send(mq,&c, sizeof(struct key_data),OS_WAIT_FOREVER);
+		}
+		os_task_msleep(500);
+	}
 }
 
 int main(void)
 {
-		os_task_t* task_key = os_task_create("task_key", Funtask_key, NULL, LIST_STK_SIZE, LIST_TASK_PRIO);
-		OS_ASSERT(task_key);
-		os_task_startup(task_key);
+	sem = os_sem_create("sem",1,3);
+	mq = os_mq_create("mq",10,5);
+	os_task_t* key_task = os_task_create("key_task", key_taskfun, NULL, LIST_STK_SIZE, LIST_TASK_PRIO);
+	os_task_t* show_task = os_task_create("show_task", show_taskfun, NULL, LIST_STK_SIZE, LIST_TASK_PRIO);
+	OS_ASSERT(key_task);
+	OS_ASSERT(show_task);
+	os_task_startup(show_task);
+	os_task_startup(key_task);
 	
-		os_task_t* task_user = os_task_create("task_user", Funtask_user, NULL, LIST_STK_SIZE, LIST_TASK_PRIO);
-		OS_ASSERT(task_user);
-		os_task_startup(task_user);
-	
-		event = os_event_create("event");	
-		
-    return 0;
+  return 0;
 }
